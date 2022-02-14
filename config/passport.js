359d4b7d43a825cf071455321300a8b16caca395
joshTLG <98714064+joshTLG@users.noreply.github.com>
2022-02-14 16:38:17 -0500

    Second Commit

--- config/passport.js
+++ config/passport.js
@@ -1,24 +1,46 @@
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-const mongoose = require('mongoose');
-const passport = require('passport');
-const User = require('../models/User');
+const mongoose = require("mongoose");
+const passport = require("passport");
+const User = require("../models/User");
 
-module.exports = function(passport) {
-    passport.use(new GoogleStrategy({
+module.exports = function (passport) {
+  passport.use(
+    new GoogleStrategy(
+      {
         clientID: process.env.GOOGLE_CLIENT_ID,
-        clientID: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
-    },
-    async (accessToken, refreshToken, profile, done) => {
-        console.log(profile)
-    }))
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "/auth/google/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const newUser = {
+          googleId: profile.id,
+          displayName: profile.displayName,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          image: profile.photos[0].value
+        };
+        try {
+            let user = await User.findOne({ googleId: profile.id }) 
 
-    passport.serializeUser((user, done) => {
-        done(null, user.id);
-    });
+            if(user) {
+                done(null, user)
+            } else {
+                user = await User.create(newUser)
+                done(null, user)
+            }
+        } catch (err) {
+            console.error(err)
+        }
+      }
+    )
+  );
 
-    passport.deserializeUser((id, done) => {
-        User.findById(id, (err, user) => done(err, user))
-    });
-}
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => done(err, user));
+  });
+};

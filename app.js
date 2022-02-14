61310aaa84ce1d60884d1fc49d9e6810d3e63855
Your Name <joshlpaige@gmail.com>
2022-02-14 11:36:16 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const passport = require('passport')
+const connectDB = require('./config/db')
+const session = require('express-session')
+
+// Load configuration
+dotenv.config({path: './config/config.env'})
+
+// Passport config
+require('./config/passport')(passport)
+
+connectDB()
+
+const app = express()
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// Handlebars
+app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+// Sessions
+
+app.use(session({ 
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    
+}))
+
+// Passports Middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.use('/', require('./routes/index'))
+const PORT = process.env.PORT || 3000
+
+
+app.listen(
+    PORT, 
+    console.log(`SERVER running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    )

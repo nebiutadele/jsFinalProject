228cd5f1a284f131019e60967a3e04bc58befa84
dledesm97 <Davidled97@gmail.com>
2022-02-15 14:25:28 -0800

    Updated the dashboard

--- config/passport.js
+++ config/passport.js
@@ -10,11 +10,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
-    },
-    async (accessToken, refreshToken, profile, done) => {
-        console.log(profile)
-    }))
+        callbackURL: "/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
@@ -22,19 +18,19 @@ module.exports = function (passport) {
           displayName: profile.displayName,
           firstName: profile.name.givenName,
           lastName: profile.name.familyName,
-          image: profile.photos[0].value
+          image: profile.photos[0].value,
         };
         try {
-            let user = await User.findOne({ googleId: profile.id }) 
+          let user = await User.findOne({ googleId: profile.id });
 
-            if(user) {
-                done(null, user)
-            } else {
-                user = await User.create(newUser)
-                done(null, user)
-            }
+          if (user) {
+            done(null, user);
+          } else {
+            user = await User.create(newUser);
+            done(null, user);
+          }
         } catch (err) {
-            console.error(err)
+          console.error(err);
         }
       }
     )
